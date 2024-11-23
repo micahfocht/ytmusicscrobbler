a4d9916068b5a78a9b7bc022312449881fd5cf53
Micah Focht <bandmfocht@gmail.com>
2024-11-23 13:08:38 -0500

    Some code style improvements.

--- scrobble.py
+++ scrobble.py
@@ -14,9 +14,7 @@ from ytmusicapi import YTMusic
 import ytmusicapi
 
 
-
-
-#Initial path assuming we are in a docker container.
+# Initial path assuming we are in a docker container.
 PATH = '/config/'
 if not os.path.exists('/.dockerenv'):
     from dotenv import load_dotenv
@@ -26,19 +24,25 @@ if not os.path.exists('/.dockerenv'):
 try:
     SLEEP = int(os.environ.get('SLEEP_TIME'))
 except TypeError:
-    #Sleep time environment variable not set, default to 45 seconds.
+    # Sleep time environment variable not set, default to 45 seconds.
     SLEEP = 45
 
-#Flask app if we need to get cookies from the user.
+# Flask app if we need to get cookies from the user.
 app = Flask(__name__)
 
 # Suppress Flask logging
 log = logging.getLogger('werkzeug')
 log.disabled = True
-def secho(text, file=None, nl=None, err=None, color=None, **styles):# pylint: disable=unused-argument
+
+
+def secho(text, file=None, nl=None, err=None, color=None, **styles):  # pylint: disable=unused-argument
     """Override logging"""
-def echo(text, file=None, nl=None, err=None, color=None, **styles):# pylint: disable=unused-argument
+
+
+def echo(text, file=None, nl=None, err=None, color=None, **styles):  # pylint: disable=unused-argument
     """Override logging"""
+
+
 click.echo = echo
 click.secho = secho
 os.environ['WERKZEUG_RUN_MAIN'] = 'True'
@@ -150,7 +154,8 @@ RESULT_PAGE = """
 </html>
 """
 
-#Kills the flask server if we need to restart
+
+# Kills the flask server if we need to restart
 @app.route('/shutdown', methods=['POST'])
 def shutdown():
     """Kills the flask server"""
@@ -160,68 +165,73 @@ def shutdown():
     shutdown_function()
     return "Shutting down..."
 
+
 @app.route('/')
 def home():
     """Loads the startup page to ask for creds"""
-    if os.path.exists(PATH+'erroredcreds.json'):
+    if os.path.exists(PATH + 'erroredcreds.json'):
         return render_template_string(BAD_CREDS)
-    if not os.path.exists(PATH+'browser.json'):
+    if not os.path.exists(PATH + 'browser.json'):
         return render_template_string(HOME_PAGE)
     return render_template_string(RESULT_PAGE)
+
 
 @app.route('/submit', methods=['POST'])
 def submit():
     """Accepts user submission for cookies"""
     user_input = request.form.get('user_input', '')
-    user_input = user_input.replace('\r','')
-    ytmusicapi.setup(filepath=PATH+ "browser.json", headers_raw=user_input)
+    user_input = user_input.replace('\r', '')
+    ytmusicapi.setup(filepath=PATH + "browser.json", headers_raw=user_input)
     print("Cookie saved")
     return render_template_string(RESULT_PAGE)
+
 
 def update_history(ytmusic: YTMusic):
     """Update listening history"""
     try:
-        #Get previously played tracks.
+        # Get previously played tracks.
         history = ytmusic.get_history()
         return history
     except ytmusicapi.exceptions.YTMusicServerError:
         try:
-            time.sleep(90)#Sleep for 90 seconds in case the issue is temporary.
-            history = ytmusic.get_history()#try to get history again using same credentials.
+            time.sleep(90)  # Sleep for 90 seconds in case the issue is temporary.
+            history = ytmusic.get_history()  # try to get history again using same credentials.
             return history
         except ytmusicapi.exceptions.YTMusicServerError:
-            if os.path.exists(PATH+'erroredcreds.json'):
-                os.remove(PATH+'erroredcreds.json')
-            os.rename(PATH+ 'browser.json', PATH+ 'erroredcreds.json')
+            if os.path.exists(PATH + 'erroredcreds.json'):
+                os.remove(PATH + 'erroredcreds.json')
+            os.rename(PATH + 'browser.json', PATH + 'erroredcreds.json')
     except Exception as e:
         print(e)
         raise e
     return history
 
+
 def login():
     """Login to youtube music"""
     try:
-        #Init the youtube music api using the existing config file.
-        ytmusic = YTMusic(PATH+ 'browser.json')
+        # Init the youtube music api using the existing config file.
+        ytmusic = YTMusic(PATH + 'browser.json')
         return ytmusic
     except ytmusicapi.exceptions.YTMusicServerError:
         try:
-            time.sleep(90)#Sleep for 90 seconds in case the issue is temporary.
-            #Init the youtube music api using the existing config file.
-            ytmusic = YTMusic(PATH+ 'browser.json')
+            time.sleep(90)  # Sleep for 90 seconds in case the issue is temporary.
+            # Init the youtube music api using the existing config file.
+            ytmusic = YTMusic(PATH + 'browser.json')
             return ytmusic
         except ytmusicapi.exceptions.YTMusicServerError as e:
-            if os.path.exists(PATH+'erroredcreds.json'):
-                os.remove(PATH+'erroredcreds.json')
-            os.rename(PATH+ 'browser.json', PATH+ 'erroredcreds.json')
+            if os.path.exists(PATH + 'erroredcreds.json'):
+                os.remove(PATH + 'erroredcreds.json')
+            os.rename(PATH + 'browser.json', PATH + 'erroredcreds.json')
             raise ytmusicapi.exceptions.YTMusicServerError from e
     except Exception as e:
         print(e)
         raise e
 
+
 def scrobble():
     """Main loop to scrobble songs to last.fm"""
-    #Connect to last.fm api using keys and credentials defined in environment variables
+    # Connect to last.fm api using keys and credentials defined in environment variables
     network = pylast.LastFMNetwork(
         api_key=os.environ.get('LASTFM_API_KEY'),
         api_secret=os.environ.get('LASTFM_API_SECRET'),
@@ -232,42 +242,45 @@ def scrobble():
     history = update_history(ytmusic)
     test = ''
     try:
-        with open(PATH+ 'history.txt', 'r',encoding='UTF-8') as file:
+        with open(PATH + 'history.txt', 'r', encoding='UTF-8') as file:
             last = file.read()
     except FileNotFoundError:
         last = ''
         print('Unable to load last song from disk.  This is expected for an new install.')
     while True:
-        #check if the last song has already been scrobbled.
+        # check if the last song has already been scrobbled.
         if last == history[0]['videoId']:
             pass
-        else:#since the last song has not been scrobbled:
-            #check that the testing variable is still the last song listened to.
+        else:  # since the last song has not been scrobbled:
+            # check that the testing variable is still the last song listened to.
             if test == history[0]['videoId']:
-                last = history[0]['videoId']#set the last song scrobbled.
-                with open(PATH+ 'history.txt', 'w',encoding='UTF-8') as f:
-                    #Theoretically this shouldn't be needed, but youtube's api occasionally doesn't
-                    #respond as expected, so this allows us to avoid sending the same track twice.
+                last = history[0]['videoId']  # set the last song scrobbled.
+                with open(PATH + 'history.txt', 'w', encoding='UTF-8') as f:
+                    # Theoretically this shouldn't be needed, but youtube's api occasionally doesn't
+                    # respond as expected, so this allows us to avoid sending the same track twice.
                     f.write(last)
-                #Send the scrobble to last.fm
-                network.scrobble(history[0]['artists'][0]['name'],history[0]['title'],
-                                 int(time.time()),history[0]['album']['name'])
-                #Print the scrobble to the log.
-                print('Scrobbling: ' + history[0]['title'] + ' at '
-                      + datetime.datetime.now().strftime('%H:%M:%S'), flush=True)
-            else:#if the song is different than the testing variable, update the testing variable.
+                # Send the scrobble to last.fm
+                network.scrobble(history[0]['artists'][0]['name'], history[0]['title'],
+                                 int(time.time()), history[0]['album']['name'])
+                # Print the scrobble to the log.
+                print('Scrobbling: ' + history[0]['title'] + ' at ' + datetime.datetime.now()
+                      .strftime('%H:%M:%S'), flush=True)
+            else:
+                # if the song is different than the testing variable, update the testing variable.
                 test = history[0]['videoId']
-        time.sleep(SLEEP)#Avoids hitting youtube's api too quickly.
+        time.sleep(SLEEP)  # Avoids hitting youtube's api too quickly.
         history = update_history(ytmusic)
-if os.path.isfile(PATH+ 'browser.json'):
+
+
+if os.path.isfile(PATH + 'browser.json'):
     scrobble()
 
 else:
     print('Please open a web browser to the web ui of this container.')
-    threading.Thread(target=lambda: app.run(host="0.0.0.0"
-                                            ,port=8000, debug=True, use_reloader=False)).start()
-    while os.path.isfile(PATH+ 'browser.json') is False:
+    threading.Thread(target=lambda: app.run(host="0.0.0.0",
+                                            port=8000, debug=True, use_reloader=False)).start()
+    while os.path.isfile(PATH + 'browser.json') is False:
         time.sleep(1)
     scrobble()
-    requests.post('http://127.0.0.1:8000/shutdown',timeout=10)
+    requests.post('http://127.0.0.1:8000/shutdown', timeout=10)
 print("An error occured, restarting at:" + datetime.datetime.now().strftime('%H:%M:%S'))
